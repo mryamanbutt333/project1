40a9c890e6fa35bf2d2540ea46c201f229f76aa6
Aqsa khan <161198603+Aqlish@users.noreply.github.com>
2024-11-12 22:42:42 +0500

    Update Heading.tsx

--- src/Components/Heading.tsx
+++ src/Components/Heading.tsx
@@ -1,9 +1,5 @@
 import React from 'react'
-interface propsType {
-    
-}
-
-const Heading: React.FC<propsType> = ({title}) => {
+function Heading (){
   return (
     <div className='text-center text-4xl pb-8'>
       <p className='border-b-4 inline-block pb-2'>My Medicine</p>
