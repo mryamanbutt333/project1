14199f5909d04cf322731bbdf02399aadfb53495
Muhammad Yaman butt <161078535+mryamanbutt333@users.noreply.github.com>
2024-11-12 23:23:54 +0500

    Update Heading.tsx

--- src/Components/Heading.tsx
+++ src/Components/Heading.tsx
@@ -1,10 +1,15 @@
-import React from 'react'
-function Heading (){
+import React from 'react';
+
+type HeadingProps = {
+  title: string;
+};
+
+const Heading: React.FC<HeadingProps> = ({ title }) => {
   return (
     <div className='text-center text-4xl pb-8'>
-      <p className='border-b-4 inline-block pb-2'>My Medicine</p>
+      <p className='border-b-4 inline-block pb-2'>{title}</p>
     </div>
-  )
+  );
 }
 
-export default Heading
+export default Heading;
