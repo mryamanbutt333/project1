ecf67d55d42a85cc570f2cd4b7ce221fc8fc7a9e
Aqsa khan <161198603+Aqlish@users.noreply.github.com>
2024-11-12 22:33:18 +0500

    Update About.tsx

--- src/Components/About.tsx
+++ src/Components/About.tsx
@@ -4,7 +4,7 @@ const About = () => {
   return (
     <div id='about' className='container pt-32'>
         <h2 className='text-4xl md:text-5xl'>About Me</h2>
-        <p className='text-gray-500 pt-4'>My medical store offers a range of high-quality medicines, healthcare products, and medical supplies to meet the needs of our community. We are committed to providing accessible and reliable healthcare solutions, ensuring our customers' well-being and convenience.</p>
+        <p className='text-gray-500 pt-4'>My medical store offers a range of high-quality medicines, healthcare products, and medical supplies to meet the needs of our community. We are committed to providing accessible and reliable healthcare solutions, ensuring our customers well-being and convenience.</p>
       
     </div>
   )
